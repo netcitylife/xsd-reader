b33648a545028fc2a4903b062519cc748d44425a
Joas1988 <Joas1988@gmail.com>
2018-09-24 18:31:01 +0200

    Annotation nodes

--- lib/xsd_reader/shared.rb
+++ lib/xsd_reader/shared.rb
@@ -120,7 +120,8 @@ module XsdReader
         "#{schema_namespace_prefix}extension" => Extension,
         "#{schema_namespace_prefix}import" => Import,
         "#{schema_namespace_prefix}simpleType" => SimpleType,
-        "#{schema_namespace_prefix}all" => SimpleType
+        "#{schema_namespace_prefix}all" => SimpleType,
+        "#{schema_namespace_prefix}annotation" => SimpleType
       }
 
       return class_mapping[n.is_a?(Nokogiri::XML::Node) ? n.name : n]

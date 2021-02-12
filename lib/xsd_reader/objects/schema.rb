cb5754fd11471dada9e55302bbbf20b151ec5775
Архипов Дмитрий <da@netcitylife.ru>
2021-02-12 16:20:57 +0200

    Restore mappable children on schema

--- lib/xsd_reader/objects/schema.rb
+++ lib/xsd_reader/objects/schema.rb
@@ -32,6 +32,16 @@ module XsdReader
       @imports ||= map_children("import")
     end
 
+    def mappable_children(xml_name)
+      result = super
+      result += import_mappable_children(xml_name) if xml_name != 'import'
+      result.to_a
+    end
+
+    def import_mappable_children(xml_name)
+      self.imports.map { |import| import.reader.schema.mappable_children(xml_name) }.flatten
+    end
+
     def import_by_namespace(ns)
       aliases = [ns, namespaces["xmlns:#{(ns || '').gsub(/^xmlns:/, '')}"]].compact
       imports.find { |import| aliases.include?(import.namespace) }
